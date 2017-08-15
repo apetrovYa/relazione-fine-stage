097f1c8e3125439bad879e4123599e18bdec2886
Andrei Petrov <Andrei.Petrov@iks.it>
2017-08-15 14:12:47 +0200

    Editare e correggere il primo capitolo., revisione di controllo

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "--------------------------------------------------------------"
+echo "-                         Pulizia                            -"
+echo "--------------------------------------------------------------"
+
+EXTENSIONS="*.sc *.acn *.acr *.alg *.aux *.bbl *.bcf *.blg *.glg *.glo *.gls *.ist *.lof *.log *.lot *.run.xml *.synctex *.toc *-frn.tex"
+
+for file in ${EXTENSIONS}; 
+do
+    rm "${file}"
+done 
