0b6dd86c623753756f850b9ec890e6b3c6776613
Andrei Petrov <Andrei.Petrov@iks.it>
2017-08-19 18:06:29 +0200

    Correggere a fondo, in revisone, il cap. 1

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ echo "--------------------------------------------------------------"
 echo "-                         Pulizia                            -"
 echo "--------------------------------------------------------------"
 
-EXTENSIONS="*.sc *.acn *.acr *.alg *.aux *.bbl *.bcf *.blg *.glg *.glo *.gls *.ist *.lof *.log *.lot *.run.xml *.synctex *.toc *-frn.tex"
+EXTENSIONS="*.sc *.acn *.acr *.alg *.aux *.bbl *.bcf *.blg *.glg *.glo *.gls *.ist *.lof *.log *.lot *.run.xml *.synctex *.toc *-frn.tex *.fls *.pdf *.fdb_latexmk *.gz" 
 
 for file in ${EXTENSIONS}; 
 do
